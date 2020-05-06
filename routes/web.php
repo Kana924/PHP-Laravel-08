41eeb52c7bab2b17c587d4a1f8f6aa5719f6d944
Kana <chi.sk.924.f@gmail.com>
2020-05-06 09:15:15 +0000

    PHP/Laravel 11

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
 
-Route::group (['prefix'=>'admin'], function() {
-    Route::get('admin/profile/create','Admin\ProfileController@add');
-    Route::get('admin/profile/edit','Admin\ProfileController @edit');
+Route::group(['prefix'=>'admin'], function() {
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController @edit');
 });
